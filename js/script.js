0288d90550c2759418e1baa4639c6b0c2cba4a6a
vwslz <vwslzk@gmail.com>
2019-10-09 13:44:36 -0400

    Update Donate Button

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@
 $('#btn-like').click(function () {
     $(this).toggleClass('fas fa-hearte');
 });
+
+$('#donation-vals').click(function() {
+  var id = $('#donation-vals input:radio:checked')[0].getAttribute("id");
+  inputVal = document.getElementById("donation-val");
+  inputVal.value = id.split("-")[1];
+})
